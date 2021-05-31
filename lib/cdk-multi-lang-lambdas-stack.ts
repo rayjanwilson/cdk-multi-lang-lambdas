ff6235dc904e4a3c750a876044d45bf3c1f98e26
Rayjan Wilson <rayjwil@amazon.com>
2021-05-31 11:00:12 -0400

    cleans up a bit

--- lib/cdk-multi-lang-lambdas-stack.ts
+++ lib/cdk-multi-lang-lambdas-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from '@aws-cdk/core';
-import { GenericPythonApp } from './python-app';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { PythonFunction } from '@aws-cdk/aws-lambda-python';
+
 export class CdkMultiLangLambdasStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new GenericPythonApp(this, 'App', { apigw: 'wat' });
+    const python_lambda = new PythonFunction(this, 'MyFunction', {
+      entry: 'lambda-fns/python3/',       // required
+      index: 'index.py',                  // optional, defaults to 'index.py'
+      handler: 'handler',                 // optional, defaults to 'handler'
+      runtime: lambda.Runtime.PYTHON_3_8, // optional, defaults to lambda.Runtime.PYTHON_3_7
+    });                                   // more details at https://docs.aws.amazon.com/cdk/api/latest/docs/aws-lambda-python-readme.html
   }
 }
