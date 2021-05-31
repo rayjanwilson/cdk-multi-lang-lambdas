eb011efbdba0b8e84f8739d194a8aa7dd1702c4f
Rayjan Wilson <rayjwil@amazon.com>
2021-05-31 12:11:12 -0400

    very simple nodejs

--- lib/cdk-multi-lang-lambdas-stack.ts
+++ lib/cdk-multi-lang-lambdas-stack.ts
@@ -7,14 +7,14 @@ export class CdkMultiLangLambdasStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const python_lambda = new PythonFunction(this, 'Lambda', {
+    const python_lambda = new PythonFunction(this, 'PyLambda', {
       entry: 'lambda-fns/python3/', // required
       index: 'index.py', // optional, defaults to 'index.py'
       handler: 'handler', // optional, defaults to 'handler'
       runtime: lambda.Runtime.PYTHON_3_8, // optional, defaults to lambda.Runtime.PYTHON_3_7
     }); // more details at https://docs.aws.amazon.com/cdk/api/latest/docs/aws-lambda-python-readme.html
 
-    const node_lambda = new NodejsFunction(this, 'Lambda', {
+    const node_lambda = new NodejsFunction(this, 'NodeLambda', {
       runtime: lambda.Runtime.NODEJS_14_X,
       entry: 'lambda-fns/nodejs/index.ts',
       handler: 'handler',
